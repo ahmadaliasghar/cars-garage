bf412035461bb4a5ddf1dcddbf3a58cf17bee359
ahmadaliasghar <ahmadali2882@yahoo.com>
2023-10-01 00:45:22 +0500

    Added Filter Section

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,16 @@ import Hero from "./Hero";
 import CustomButton  from "./CustomButton"
 import Navbar from "./Navbar" 
 import Footer from "./Footer" 
+import Searchbar from "./SearchBar" 
+import CustomFilter from "./CustomFilter" 
+import SearchManufacturer from "./SearchManuafacturer"
+
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    Searchbar,
+    CustomFilter,
+    SearchManufacturer
 }
